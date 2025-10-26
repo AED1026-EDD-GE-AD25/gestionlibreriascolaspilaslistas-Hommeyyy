441eb5dbc970d9b5d76fcc2389a58e2429e1fc73
Ochoa Sauceda Hommey Josue <L24170661@culiacan.tecnm.mx>
2025-10-26 06:46:20 +0000

    avance menu con errores

--- miPrincipal/modelo/Pedido.java
+++ miPrincipal/modelo/Pedido.java
@@ -3,8 +3,28 @@ package miPrincipal.modelo;
 import utilerias.Fecha;
 
 public class Pedido {
+   
     private Libro libro;
     private Fecha fechaPedido;
 
    
+    public Pedido(Libro libro, Fecha fecha){
+        this.libro = libro;
+        this.fechaPedido = fecha;
+    }
+
+   
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public Fecha getFechaPedido() {
+        return fechaPedido;
+    }
+
+    
+    @Override
+    public String toString(){
+        return "Pedido -> Libro: [" + libro + "], Fecha: " + fechaPedido;
+    }
 }
